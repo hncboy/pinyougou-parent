f2b208a39f3fb6bcfbf63a2f7feae9517cf06a65
hncboy <619452863@qq.com>
2018-07-10 14:01:48 +0800

    完成广告类型，广告管理，网站首页-广告展示，网站首页-缓存广告数据。

--- pinyougou-dao/src/main/java/com/pinyougou/mapper/TbContentCategoryMapper.java
+++ pinyougou-dao/src/main/java/com/pinyougou/mapper/TbContentCategoryMapper.java
@@ -2,9 +2,8 @@ package com.pinyougou.mapper;
 
 import com.pinyougou.pojo.TbContentCategory;
 import com.pinyougou.pojo.TbContentCategoryExample;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface TbContentCategoryMapper {
     int countByExample(TbContentCategoryExample example);
--- pinyougou-dao/src/main/java/com/pinyougou/mapper/TbContentMapper.java
+++ pinyougou-dao/src/main/java/com/pinyougou/mapper/TbContentMapper.java
@@ -2,9 +2,8 @@ package com.pinyougou.mapper;
 
 import com.pinyougou.pojo.TbContent;
 import com.pinyougou.pojo.TbContentExample;
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface TbContentMapper {
     int countByExample(TbContentExample example);
