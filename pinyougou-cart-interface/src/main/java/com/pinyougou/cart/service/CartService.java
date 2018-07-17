cf2e599b039ac0d943613c0b3ecdf209e7ee44f8
hncboy <619452863@qq.com>
2018-07-17 11:01:33 +0800

    完成单购物车解决方案：Cookie存储购物车，Redis存储购物车

---
+++ pinyougou-cart-interface/src/main/java/com/pinyougou/cart/service/CartService.java
@@ -0,0 +1,46 @@
+package com.pinyougou.cart.service;
+
+import com.pinyougou.pojogroup.Cart;
+
+import java.util.List;
+
+/**
+ * 购物车服务接口
+ */
+public interface CartService {
+
+    /**
+     * 添加商品到购物车
+     *
+     * @param list
+     * @param itemId
+     * @param num
+     * @return
+     */
+    List<Cart> addGoodsToCartList(List<Cart> list, Long itemId, Integer num);
+
+    /**
+     * 从redis中提取购物车
+     *
+     * @param username
+     * @return
+     */
+    List<Cart> findCartListFromRedis(String username);
+
+    /**
+     * 将购物车列表存入redis
+     *
+     * @param username
+     * @param cartList
+     */
+    void saveCartListToRedis(String username, List<Cart> cartList);
+
+    /**
+     * 合并购物车
+     *
+     * @param cartList1
+     * @param cartList2
+     * @return
+     */
+    List<Cart> mergeCartList(List<Cart> cartList1, List<Cart> cartList2);
+}
